3c19ef280cf21cfc2954304ffc0c7335eea594c1
Zhanarys <admin>
2024-10-09 16:13:45 +0500

    week 4

---
+++ lab4/lab4.sql
@@ -0,0 +1,22 @@
+-- task 3:
+insert into warehouses (location, capacity)
+values
+('Chicago', 3),
+('Chicago', 4),
+('New York', 7),
+('Los Angeles',2),
+('San Francisco', 8);
+
+insert into boxes (code, contents, value, warehouse)
+values
+('0MN7', 'Rocks', 180, 3),
+('4H8P', 'Rocks', 250, 1),
+('4RT3', 'Scissors', 190, 4),
+('7G3H', 'Rocks', 200, 1),
+('8JN6', 'Papers', 75, 1),
+('8Y6U', 'Papers', 50, 3),
+('9J6F', 'Papers', 175, 2),
+('LL08', 'Rocks', 140, 4),
+('P0H6', 'Scissors', 125, 1),
+('P2T6', 'Scissors', 150, 2),
+('TU55', 'Papers', 90, 5);
---
+++ lab4/task.sql
@@ -0,0 +1,79 @@
+-- task 1:
+create database lab4;
+
+
+-- task 2:
+create table Warehouses(
+    code serial primary key,
+    location varchar(255),
+    capacity integer
+);
+
+create table Boxes(
+    code char(4),
+    contents varchar(255),
+    value real,
+    warehouse integer references Warehouses(code);
+);
+
+
+-- task 4:
+select * from warehouses;
+
+
+-- task 5:
+select * from boxes where value > 150;
+
+
+-- task 6:
+select distinct contents from boxes;
+
+
+-- task 7:
+select warehouse, count(*) as num_box
+from Boxes
+group by warehouse;
+
+
+-- task 8:
+select warehouse, count(*) as num_box
+from Boxes
+group by warehouse
+having count(*)>2;
+--агрегатные функции
+
+-- task 9:
+insert into warehouses (location, capacity)
+values
+('New York', 3);
+
+
+-- task 10:
+insert into boxes (code, contents, value, warehouse)
+values
+('H5RT', 'Papers', 200, 2);
+
+
+-- task 11:
+update  boxes
+set value = 0.85 * value
+where value = (select value from boxes order by value desc limit 1 offset 2);
+
+
+-- task 12:
+delete from boxes
+where value < 150;
+
+
+-- task 13:
+delete from Boxes
+where warehouse in (
+    select code FROM Warehouses
+    WHERE location = 'New York'
+)
+RETURNING *;
+
+
+
+
+
